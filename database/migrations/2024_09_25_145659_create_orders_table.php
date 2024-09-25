ee4762bd1941d3d4727c892c458bb84121082d3e
Erdal <erdalfidan1508@gmail.com>
2024-09-25 15:58:44 +0000

    feat: migration ve model dosyalari olusturuldu

--- database/migrations/2024_09_25_145659_create_orders_table.php
+++ database/migrations/2024_09_25_145659_create_orders_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'staff']);
-            $table->rememberToken();
+            $table->foreignId('table_id')->constrained()->onDelete('restrict');
+            $table->foreignId('user_id')->constrained()->onDelete('restrict');
+            $table->timestamp('order_date')->useCurrent();
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
--- database/migrations/2024_09_25_145734_create_expenses_table.php
+++ database/migrations/2024_09_25_145734_create_expenses_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'staff']);
-            $table->rememberToken();
+            $table->string('description');
+            $table->decimal('amount', 10, 2);
+            $table->date('date');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses');
     }
 };
