a5423ede58a8723c6e947aa619ff53f5cdd0d943
Erdal <erdalfidan1508@gmail.com>
2024-09-27 10:46:49 +0000

    feat: temel api root olusturuldu

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TableController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderItemController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +18,48 @@ use App\Http\Controllers\Api\TableController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/tables', [TableController::class, 'index']);
-Route::get('/tables/{id}', [TableController::class, 'show']);
+
+
+Route::prefix('tables')->group(function () {
+    Route::get('/', [TableController::class, 'index']);
+    Route::get('/{id}', [TableController::class, 'show']);
+    Route::post('/', [TableController::class, 'store']);
+    Route::put('/{id}', [TableController::class, 'update']);
+    Route::delete('/{id}', [TableController::class, 'destroy']);
+});
+
+Route::prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::get('/{id}', [OrderController::class, 'show']);
+    Route::post('/', [OrderController::class, 'store']);
+    Route::put('/{id}', [OrderController::class, 'update']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
+});
+
+Route::prefix('order-items')->group(function () {
+    Route::get('/', [OrderItemController::class, 'index']);
+    Route::get('/{id}', [OrderItemController::class, 'show']);
+    Route::post('/', [OrderItemController::class, 'store']);
+    Route::put('/{id}', [OrderItemController::class, 'update']);
+    Route::delete('/{id}', [OrderItemController::class, 'destroy']);
+});
+
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::post('/', [ProductController::class, 'store']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::prefix('payments')->group(function () {
+    Route::get('/', [PaymentController::class, 'index']);
+    Route::get('/{id}', [PaymentController::class, 'show']);
+    Route::post('/', [PaymentController::class, 'store']);
+    Route::put('/{id}', [PaymentController::class, 'update']);
+    Route::delete('/{id}', [PaymentController::class, 'destroy']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
